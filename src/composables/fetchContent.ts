a78eb9ca29d18d9a15c411f7dc88a4da1e0c9ad2
Alex Bowers <alex.bowers@bluebaytravel.co.uk>
2022-05-05 15:28:05 +0100

    Remove test code and fix function name

--- src/composables/fetchContent.ts
+++ src/composables/fetchContent.ts
@@ -11,12 +11,11 @@ const Storyblok = new StoryblokClient({
 
 export function fetchProduct(slug: string, cms: string): Promise<Product> | Product {
     const services: { [key: string]: Promise<Product>} = {
-        storyblok: storyBlokProduct(slug)
+        storyblok: fetchStoryblokProduct(slug)
     }
 
     if (cms in services) {
-        return Promise.resolve(services[cms].then(data => createProduct(data, cms)))
-        .then(product => product)
+        return services[cms].then(data => createProduct(data, cms))
     }
 
     return dataFromCMS.products[slug]
@@ -26,7 +25,7 @@ export function fetchBlog(slug: string): Blog {
     return dataFromCMS.blogs[slug]
 }
 
-function storyBlokProduct(slug: string) {
+function fetchStoryblokProduct(slug: string) {
     return Storyblok.get(`cdn/stories/products/${slug}`, {
         version: 'draft',
     }).then(({ data }) => {
